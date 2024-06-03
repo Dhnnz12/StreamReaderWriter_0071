e8f5702e3d007b0ce14a87915f6734e9140bbf12
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-06-03 15:03:49 +0700

    menambahkan catch untuk blok akan di eksekusi

--- ManualExecption/Manual.cpp
+++ ManualExecption/Manual.cpp
@@ -5,5 +5,16 @@ int main()
 {
     try{
         cout << "Selamat Belajar di prodi TI UMY " << endl;
+
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
+    catch(int a){
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+
+    catch(...){
+        cout << "Default Pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
 }
