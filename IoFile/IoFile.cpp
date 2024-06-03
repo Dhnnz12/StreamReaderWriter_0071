50e67cf6095b9e712b0df10de2eddbd5cdb5ec7a
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-06-03 14:30:32 +0700

    melakukan perulangan setiap baris

--- IoFile/IoFile.cpp
+++ IoFile/IoFile.cpp
@@ -24,4 +24,16 @@ int main(){
 outfile.close();
 
 ifstream infile;
+
+infile.open("contohfile.txt");
+
+cout << endl << ">= Membuka dan membaca file " << endl;
+
+if (infile.is_open())
+{
+    while (getline(infile, baris))
+    {
+        cout << baris << '\n';
+    }
+}
 }
